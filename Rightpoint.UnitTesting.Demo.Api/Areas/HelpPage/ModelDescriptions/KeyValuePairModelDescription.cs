f4c16beb2c3e6f3125f032a893968d2d27dea157
Andrew Varnon <avarnon@rightpoint.com>
2017-08-26 13:15:35 -0400

    Add project files.

---
+++ Rightpoint.UnitTesting.Demo.Api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Rightpoint.UnitTesting.Demo.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Rightpoint.UnitTesting.Demo.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Rightpoint.UnitTesting.Demo.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
